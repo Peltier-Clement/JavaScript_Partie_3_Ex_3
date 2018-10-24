27cb13d4c47deded72881c9a40ee4688be4ab2d6
nisted <clement.p10@laposte.net>
2018-10-24 10:44:52 +0200

    premier depot

---
+++ script.js
@@ -0,0 +1,7 @@
+var firstName = prompt ("Entrez votre prénom:");
+alert ("Bonjour" + firstName);
+// if (isNaN(firstName)) {
+//   alert ("Bonjour :" + firstName);
+// }else{
+//   alert("Erreur!");
+// }
